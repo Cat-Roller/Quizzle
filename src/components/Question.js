c6cd6b7c9e4ca9e1b5fcbc71852bad06ed82a452
Mels <mels.tadevosyan@gmail.com>
2022-09-23 19:01:20 +0400

    Answer buttons beacame better

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,15 +2,18 @@ import React from "react";
 
 export default function Question(props) 
 {
-    
+    const RigthAnswerIndex=props.rigthAnswerId
+    const RightAnswer=props.answers[0]
+    props.answers[0]=props.answers[RigthAnswerIndex]
+    props.answers[RigthAnswerIndex]=RightAnswer
     return(
         <div className="question">
             <h4 className="ask">{props.question}</h4>
             <div className="answers">
-                <button className="answer">{props.answer1}</button>
-                <button className="answer">{props.answer2}</button>
-                <button className="answer">{props.answer3}</button>
-                <button className="answer">{props.answer4}</button>
+                <button className="answer">{props.answers[0]}</button>
+                <button className="answer">{props.answers[1]}</button>
+                <button className="answer">{props.answers[2]}</button>
+                <button className="answer">{props.answers[3]}</button>
             </div>
             <hr className="solidHr"/>
         </div>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -3,21 +3,39 @@ import Question from "./Question.js"
 
 export default function QuestionList()
 {
-        const [questions, setQuestions] =React.useState([])
-        React.useEffect(() => {
-                fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
-                    .then(res => res.json())
-                    .then(data =>setQuestions(data.results))
-            }, [])
-            
-          const List =  questions.map(question => <Question question={question.question} 
-                answer1={question.correct_answer} answer2={question.incorrect_answers[0]} 
-                answer3={question.incorrect_answers[1]} answer4={question.incorrect_answers[2]} />) 
+       const [questions, setQuestions] =React.useState([])
+      const [endQuiz,setEndQuiz] = React.useState(false)
+      React.useEffect(() => {
+              fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
+                  .then(res => res.json())
+                  .then(data =>setQuestions(data.results))
+          }, [])
+                      
+        const List =  questions.map(question => <Question question={question.question} 
+              answers={[
+              question.correct_answer,
+              question.incorrect_answers[0],
+              question.incorrect_answers[1],
+              question.incorrect_answers[2]]}
+              key={question.question}
+              id={question.question}
+              rigthAnswerId={Math.floor(Math.random() * 4)}
+              />) 
 
-    return(
-        <div className="QuestionList">
-          {List}
-         <button className="Replay">See Result</button>
-        </div>
-    )
+             function endGame()
+             {
+              const rightAnswers=0
+              console.log(List[0])
+              setEndQuiz(true)
+             }
+        
+  return(
+    
+      <div className="QuestionList">
+        {List}
+        {endQuiz ? <button className="Replay">Replay</button> 
+        : <button className="Replay" onClick={endGame}>See Result</button>}
+      </div>
+
+  )
 }
